50a2eb71565686054bd320816c140b6e8fa6cba7
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2020-06-05 09:31:29 -0700

    add fit summary to test assert

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -8,7 +8,7 @@ KEYS = ['qi', 'qc', 'xa', 'a', 'f0']
 def check_guess(f, result, guess):
     for parameter, value in guess.items():
         message = f"The guess for {parameter} is incorrect. Used: {value}, Reported: {result['guess'][parameter]}."
-        assert value == result['guess'][parameter], message
+        assert value == result['guess'][parameter], message + "\n" + result['summary']
     assert_allclose(lf.model(f, **result['guess']), lf.model(f, **guess))
 
 
@@ -19,9 +19,10 @@ def test_fit_iq(f, model, guess, parameters, data):
     beta = parameters.get('beta', 0.0)
     result = lf.fit(f, data.real, data.imag, nonlinear=True if a else False,
                     imbalance=True if np.any([alpha != 1.0, beta]) else False, **guess)
-    assert result['success'], "The fit did not succeed."
+    assert result['success'], result['summary']
     # check that fit is correct to ~10% for the parameters we care about
-    assert_allclose([parameters[key] for key in KEYS], [result[key] for key in KEYS], rtol=1e-1, atol=1e-6)
+    assert_allclose([parameters[key] for key in KEYS], [result[key] for key in KEYS], rtol=1e-1, atol=1e-6,
+                    err_msg=result['summary'])
     check_guess(f, result, guess)
 
 
@@ -34,5 +35,6 @@ def test_fit_z(f, model, guess, parameters, data):
                     imbalance=True if np.any([alpha != 1.0, beta]) else False, **guess)
     assert result['success'], "The fit did not succeed."
     # check that fit is correct to ~10% for the parameters we care about
-    assert_allclose([parameters[key] for key in KEYS], [result[key] for key in KEYS], rtol=1e-1, atol=1e-6)
+    assert_allclose([parameters[key] for key in KEYS], [result[key] for key in KEYS], rtol=1e-1, atol=1e-6,
+                    err_msg=result['summary'])
     check_guess(f, result, guess)
