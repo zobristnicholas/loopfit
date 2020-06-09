93836caa85b3d0f33f7898f20657bd57e6297391
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2020-06-09 11:48:17 -0700

    test numerical jacobian fitting

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -49,3 +49,20 @@ def test_fit_vector_sigma(f, guess, parameters, data, sigma):
     result = lf.fit(f, data.real, data.imag, nonlinear=True if a else False, sigma=sigma,
                     imbalance=True if np.any([alpha != 1.0, beta]) else False, **guess)
     check_fit(f, result, guess, parameters)
+
+
+def test_fit_numerical(f, random):
+    """Test that the fit works using the numerical Jacobian."""
+    # Don't test with the full batch of parameters because this method is less robust and slower
+    p = {'f0': 4.0004, 'qi': 400000, 'qc': 90000, 'xa': 0.0, 'a': 1.0, 'alpha': 1.1, 'beta': 0.3,
+         'gamma': 0.1, 'delta': 2.0, 'gain0': 2.0, 'gain1': -5000.0, 'gain2': 1250000.0, 'phase0': 4.1,
+         'phase1': -4300.0, 'decreasing': True, 'fm': 4.0004}
+    m = lf.model(f, **p)
+    s = 0.02 + 0.02j
+    n = (random.normal(0, s.real, m.shape) + 1j * random.normal(0, s.imag, m.shape)).astype(m.dtype)
+    d = m + n
+    g = lf.guess(f, z=d, beta=0 if np.abs(p['beta']) < np.pi / 2 else np.pi, gamma=p['gamma'], delta=p['delta'],
+                 nonlinear=True if p['a'] else False, decreasing=p['decreasing'])
+    r = lf.fit(f, d.real, d.imag, nonlinear=True if p['a'] else False, sigma=s, numerical=True,
+               imbalance=True if np.any([p['alpha'] != 1.0, p['beta']]) else False, **g)
+    check_fit(f, r, g, p)
